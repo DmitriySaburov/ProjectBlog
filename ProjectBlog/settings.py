cd410b37b2a266270be6e9a4a1ba097ce535b19a
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-04-29 18:01:36 +0300

    поработал с url и шаблонами

--- ProjectBlog/settings.py
+++ ProjectBlog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ProjectBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "temolates"],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
