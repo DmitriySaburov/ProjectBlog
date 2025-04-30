f5d8dfc25234f25a15fae57ed47aa852dfd20e3f
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-04-30 18:14:36 +0300

    добавил пагинацию

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 
 
 def post_list(request):
-    posts = Post.published.all()
-    context = {"posts": posts}
-    return render(request=request,
-                  template_name="blog/post/list.html",
-                  context=context)
+    post_list = Post.published.all()
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):
