05b7c677519398d6d86b1b8f90f26e52dbd218ae
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-05-10 20:30:39 +0300

    добавил пользовательский шаблонный фильтр

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,8 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+
+import markdown
 
 from ..models import Post
 
@@ -18,8 +21,14 @@ def show_latest_posts(count=5):
     latest_posts = Post.published.order_by("-publish")[:count]
     return {"latest_posts": latest_posts}
 
+
 @register.simple_tag
 def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count("comments")
     ).exclude(total_comments=0).order_by("-total_comments")[:count]
+
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
