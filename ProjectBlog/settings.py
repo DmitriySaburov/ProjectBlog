c1e71b71147a3e1e278cb380901845c93040e970
DmitriySaburov <dmitriy.saburov94@mail.ru>
2025-05-13 17:35:13 +0300

    перенес БД на PostgreSQL

--- ProjectBlog/settings.py
+++ ProjectBlog/settings.py
@@ -64,10 +64,23 @@ WSGI_APPLICATION = 'ProjectBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+"""
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "blog",
+        "USER": "blog",
+        "PASSWORD": "blogpassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
